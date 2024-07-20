f89575a61191ab3f1bdaa37b5e56b20ca96031d3
subhasish <subhasishpraharaj52@gmail.com>
2024-07-20 14:10:42 +0530

    Update .gitignore and remove ignored files from index

--- app/schemas.py
+++
@@ -1,9 +0,0 @@
-# app/schemas.py
-
-from marshmallow import Schema, fields
-
-class EmployeeSchema(Schema):
-    id = fields.Int(dump_only=True)
-    name = fields.Str(required=True)
-    age = fields.Int(required=True)
-    department = fields.Str(required=True)
